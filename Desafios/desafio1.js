1d96cb94b729f812a877a91f3f0503c2996f1ab9
“JoseLuisSep85“ <jlsv@outlook.cl>
2023-08-23 14:18:02 -0400

    correciones en comentarios

--- Desafios/desafio1.js
+++ Desafios/desafio1.js
@@ -43,17 +43,23 @@ const productManager = new ProductManager()
 
 //agregar productos
 
+//producto 1
 productManager.addProduct("Sin titulo", "Sin descripcion", 200, "Sin imagen", "abc123", 25)
+
+//producto 2 con code repetido
 productManager.addProduct("Sin titulo 2", "Sin descripcion 2", 300, "Sin imagen 2", "abc123", 25)
+
+//producto 3 con code distinto
 productManager.addProduct("Sin titulo 3", "Sin descripcion 3", 400, "Sin imagen 3", "abc1234", 25)
-productManager.addProduct("Sin titulo 3", 400, 25)
+
+//producto 4 con datos faltantes
+productManager.addProduct("Sin titulo 4", 400, 25)
+
 
 //obtener productos
-
 const productos = productManager.getProducts()
 console.log("Productos: ", productos);
 
 // buscar productos por id
-
 const buscarProductos = productManager.getProductById(1)
 console.log(buscarProductos);
