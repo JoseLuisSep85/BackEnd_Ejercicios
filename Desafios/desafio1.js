5e7231629af60b7ff57b60f11cc15a1adecb629e
“JoseLuisSep85“ <jlsv@outlook.cl>
2023-08-22 23:10:08 -0400

    Desafio_01

---
+++ Desafios/desafio1.js
@@ -0,0 +1,25 @@
+
+class ProductManager {
+    constructor() {
+        this.products = [];
+        
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) { 
+
+    }
+
+    getProducts() {
+        return this.products
+    }
+        
+
+    getProductById() {
+        
+    }
+
+}
+
+const producto1 = new ProductManager("hola");
+
+console.log(producto1);
